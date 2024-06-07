c7b3c9c6b36a10d32ce59419a04f379e47436f83
johnmichaaael <nvrmnd1991@gmail.com>
2024-06-07 09:21:16 +0800

    Working delete.php

--- crud/delete.php
+++ crud/delete.php
@@ -17,7 +17,7 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
         // Attempt to execute the prepared statement
         if($stmt->execute()){
             // Records deleted successfully. Redirect to landing page
-            header("location: index.php");
+            header("location: crud.php");
             exit();
         } else{
             echo "Oops! Something went wrong. Please try again later.";
@@ -64,7 +64,7 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
                             <p>Are you sure you want to delete this employee record?</p>
                             <p>
                                 <input type="submit" value="Yes" class="btn btn-danger">
-                                <a href="index.php" class="btn btn-secondary ml-2">No</a>
+                                <a href="crud.php" class="btn btn-secondary ml-2">No</a>
                             </p>
                         </div>
                     </form>
