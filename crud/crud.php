891d89cdcc551b4ccbd11a4a94806edd34869c19
johnmichaaael <nvrmnd1991@gmail.com>
2024-06-18 15:06:32 +0800

    Updated index.php

--- crud/crud.php
+++ crud/crud.php
@@ -39,6 +39,10 @@
       </li>
     </ul>
   </div>
+  <p>
+        <a href="../reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
+    </p>
 </nav>
 <body>
     <div class="wrapper">
